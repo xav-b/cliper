8d8132cd7eaf9d6e58878f96530bb531904b11b5
hackliff <xavier.bruhiere@gmail.com>
2017-03-06 22:03:44 +0100

    fix bootstrap support for linux

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -7,17 +7,19 @@ readonly LATEST_VERSION="0.1.1"
 
 function download_binary() {
   local version="$1"
-  local platform=darwin-amd64
+  local os="$(uname | tr '[:upper:]' '[:lower:]')"
+  local arch="amd64"
 
-  echo "downloading binary (${PROJECT}-${platform})..."
+  echo "downloading binary (${PROJECT}-${os}-${arch})..."
   curl \
     -ksL \
     -o ${BIN_PATH}/${PROJECT} \
-    https://github.com/${GH_AUTHOR}/${PROJECT}/releases/download/v${version}/${PROJECT}-${platform}
+    https://github.com/${GH_AUTHOR}/${PROJECT}/releases/download/v${version}/${PROJECT}-${os}-${arch}
 
-  echo "make it executable (${BIN_PATH}/${PROJECT})"
+  echo "making it executable (${BIN_PATH}/${PROJECT})"
   chmod +x ${BIN_PATH}/${PROJECT}
 }
 
 download_binary ${CLIPER_VERSION:-"${LATEST_VERSION}"}
+echo "done: $(command -v cliper)"
 
